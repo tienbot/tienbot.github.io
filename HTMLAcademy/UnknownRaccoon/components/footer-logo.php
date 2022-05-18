2d7c0ca6e9fb20c98b64fe22b48d049ebfdd852e
tienbot <72535483+tienbot@users.noreply.github.com>
2022-05-19 01:48:11 +0900

    Delete HTMLAcademy/UnknownRaccoon directory

--- HTMLAcademy/UnknownRaccoon/components/footer-logo.php
+++
@@ -1,9 +0,0 @@
-</main>
-  <footer class="page-footer page-footer--logo">
-    <div class="container">
-      <p>© <?= $name ?>, 2020</p>
-    </div>
-  </footer>
-  <script src="script.js"></script>
-</body>
-</html>
--- HTMLAcademy/UnknownRaccoon/components/status.php
+++
@@ -1,4 +0,0 @@
-<section class="status container">
-  <h2 class="section-title">Мой статус</h2>
-  <p>#яжверстальщик!</p>
-</section>
